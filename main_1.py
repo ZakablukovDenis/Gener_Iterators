4341b2105a2d9c572995a5edbdeb56157a2ab57b
Denis Zakablukov <zakablukov_77@inbox.ru>
2023-01-14 18:37:11 +0400

    Start_Commit_07-01-2023

---
+++ main_1.py
@@ -0,0 +1,45 @@
+class FlatIterator:
+
+    def __init__(self, list_of_lists):
+        self.list_of_lists = list_of_lists
+        self.cursor = -1
+        self.list_len = len(self.list_of_lists)
+
+    def __iter__(self):
+        self.cursor += 1
+        self.nest_cursor = 0
+        return self
+
+    def __next__(self):
+        if self.nest_cursor == len(self.list_of_lists[self.cursor]):
+            iter(self)
+
+        if self.cursor == self.list_len:
+            raise StopIteration
+
+        self.nest_cursor += 1
+        item = self.list_of_lists[self.cursor][self.nest_cursor - 1]
+
+        return item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
